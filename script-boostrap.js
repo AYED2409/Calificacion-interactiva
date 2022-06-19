0db150d904916a16f7f9c7edc104a2c1e98b62a4
AYED2409 <ayed2409@gmail.com>
2022-06-19 02:34:34 +0200

    terminado

--- script-boostrap.js
+++ script-boostrap.js
@@ -1,70 +1,18 @@
 const num = document.querySelectorAll('.num')
-const contenedor =document.querySelector('.contenedor')
-const card =document.querySelector('.card')
-const parrCard = document.querySelector('.card-text')
-const enlaces = document.querySelectorAll('a')
-const enviar =document.querySelector('.enviar')
+//const contenedor =document.querySelector('.contenedor')
+//const card =document.querySelector('.card')
+//const parrCard = document.querySelector('.card-text')
+//const enlaces = document.querySelectorAll('a')
+//const enviar =document.querySelector('.enviar')
 const estrella =document.querySelector('.estrella')
+const seleccion =document.querySelector('#seleccion')
+const tituloTarjeta=document.querySelector('.card-title')
+const textoTarjeta=document.querySelector('.card-text')
+const cardBody =document.querySelector('.card-body')
+const container =document.querySelector('.container')
+const enviar =document.querySelector('.enviar')
 let valor=0
 
-/*
-const FondoEstrella = () =>{
-    estrella.style.backgroundColor="#262F38"
-    estrella.style.padding="12px"
-    estrella.style.borderRadius="25px"
-    estrella.style.marginBottom="25px"
-}
-
-const cambiarColorGrisOscuro = () =>{
-    num.forEach((e) =>{
-        e.style.backgroundColor='#262F38'
-        e.style.padding="15px"
-        e.style.margin="4px"
-    })
-}
-
-const cambiarAltoContenedor = () =>{
-    contenedor.style.height="930px"
-    contenedor.style.background="#141519"
-    
-}
-
-const cambiarColorTarjeta = () =>{
-    card.style.background="linear-gradient(0deg, #171E28 70% ,#363d45,#7c8998)"
-    card.style.paddingBottom="15px"
-    card.style.borderRadius="25px"
-}
-
-const cambiarColorTexto = () =>{
-    parrCard.style.color="#8C939D"
-    parrCard.style.fontSize="15px"
-    parrCard.style.fontFamily= "font-family: 'Overpass', sans-serif";
-}
-
-const resEnlaces = () =>{
-    enlaces.forEach((e)=>{
-        e.style.textDecoration="none"
-        e.style.color="#515964"
-    })
-}
-const colorEnviar = () =>{
-    enviar.style.color="white"
-    enviar.style.fontSize="13px"
-    enviar.style.backgroundColor="#FC7613"
-    enviar.style.fontWeight = "bold"
-}
-
-const cambiarColor = (e) =>{
-    console.log(e.target)
-}
-cambiarColorGrisOscuro()
-cambiarAltoContenedor()
-cambiarColorTarjeta()
-cambiarColorTexto()
-resEnlaces()
-colorEnviar()
-FondoEstrella()
-*/
 const borrarGrisClaro = () =>{
     num.forEach((e) =>{
         e.classList.remove('grisClaro')
@@ -74,8 +22,6 @@ const borrarGrisClaro = () =>{
 
 document.addEventListener("click", (e)=>{
     if(e.target.matches(".num1")){
-        //console.log("voto 1") 
-        //cambiarColorGrisOscuro()
         borrarGrisClaro()
         num[0].classList.add('grisClaro')
         num[0].classList.remove('grisOscuro')
@@ -83,8 +29,6 @@ document.addEventListener("click", (e)=>{
         
     }
      if(e.target.matches(".num2")){
-        //console.log("voto 2")
-        //cambiarColorGrisOscuro()
         borrarGrisClaro()
         num[1].classList.add('grisClaro')
         num[1].classList.remove('grisOscuro')
@@ -92,8 +36,6 @@ document.addEventListener("click", (e)=>{
         
     }
     if(e.target.matches(".num3")){
-        //console.log("voto 3")
-        //cambiarColorGrisOscuro()
         borrarGrisClaro()
         num[2].classList.add('grisClaro')
         num[2].classList.remove('grisOscuro')
@@ -101,8 +43,6 @@ document.addEventListener("click", (e)=>{
    
     }
      if(e.target.matches(".num4")){
-        //console.log("voto 4")
-        //cambiarColorGrisOscuro()
         borrarGrisClaro()
         num[3].classList.add('grisClaro')
         num[3].classList.remove('grisOscuro')
@@ -110,8 +50,6 @@ document.addEventListener("click", (e)=>{
     
     }
     if(e.target.matches(".num5")){
-        //console.log("voto 5")
-        //cambiarColorGrisOscuro()
         borrarGrisClaro()
         num[4].classList.add('grisClaro')
         num[4].classList.remove('grisOscuro')
@@ -119,9 +57,26 @@ document.addEventListener("click", (e)=>{
     }
     if(e.target.matches(".enviar")){
         console.log("selecciono: "+valor)
+        pintarEnviado()
         if(valor===0){
             window.alert("Debe seleccionar un valor");
         }
     }
 })
 
+const pintarEnviado = () =>{
+    estrella.src="./images/illustration-thank-you.svg"
+    estrella.classList.add("rounded")
+    estrella.classList.add("mx-auto")
+    estrella.classList.add("d-block")
+    estrella.classList.add("mt-4")
+    seleccion.textContent="You selected "+ valor + " out of 5 "
+    seleccion.classList.add('grisOscuro')
+    tituloTarjeta.textContent=" Thank you!"
+    tituloTarjeta.classList.add("text-center")
+    tituloTarjeta.classList.add("mt-4")
+    textoTarjeta.textContent="We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!"
+    textoTarjeta.classList.add("text-center")
+    cardBody.removeChild(container)
+    cardBody.removeChild(enviar)
+}
